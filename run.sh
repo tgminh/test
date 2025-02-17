964aa55eea4db68703cd662442e218bfb4f7ca6e
Trần Gia Minh <115785811+tgminh@users.noreply.github.com>
2025-02-17 22:36:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-names=("longdress" "loot" "redandblack" "soldier")
-no=(1051 1000 1450 536)
-tile_sizes=("2,2,2" "3,3,3" "4,4,4")  
+names=("redandblack" "soldier")
+no=(1450 536)
+tile_sizes=("2,2,2" "3,3,3" "4,4,4")
 mkdir test
 
 for tile_size in "${tile_sizes[@]}"; 
@@ -8,12 +8,14 @@ do
     for i in {0..3}; 
     do  
     mkdir test/${names[$i]}
-
+    mkdir test/${names[$i]}/
 
         for j in {0..299}; 
         do  
+            num=$(printf "%04d" $((nos[i] + j)))
+            filename="8i_dataset/${names[i]}/Ply/${names[i]}_vox10_${num}.ply"
             filename="8i_dataset/${names[$i]}/Ply/${names[$i]}_vox10_$((${no[$i]} + $j)).ply"
-            output="test/${names[$i]}/tile_${tile_size}_%04d.ply"
+            output="test/${names[$i]}/$num/tile__${tile_size}_%04d.ply"
             ./bin/pcp -i $filename -o $output -t $tile_size
         done
     done
