c188350bb784c8012abfa9025e7494bdeb7b263a
Trần Gia Minh <115785811+tgminh@users.noreply.github.com>
2025-02-17 23:05:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
-names=("redandblack" "soldier")
-no=(1450 536)
-tile_sizes=("2,2,2" "3,3,3" "4,4,4")
-mkdir test
+names=("longdress" "loot" "redandblack" "soldier")
+no=(1051 1000 1450 536)
+tile_sizes=("2,2,2" "3,3,3" "4,4,4")  # Use spaces instead of commas
+mkdir -p test
 
-for tile_size in "${tile_sizes[@]}"; 
-do  
-    for i in {0..3}; 
-    do  
-    mkdir test/${names[$i]}
-    mkdir test/${names[$i]}/$num
-
-        for j in {0..299}; 
-        do  
-            num=$(printf "%04d" $((nos[i] + j)))
-            filename="8i_dataset/${names[i]}/Ply/${names[i]}_vox10_${num}.ply"
-            output="test/${names[$i]}/$num/tile__${tile_size}_%04d.ply"
-            ./bin/pcp -i $filename -o $output -t $tile_size
+for tile_size in "${tile_sizes[@]}"; do  
+    for i in {0..3}; do  
+    mkdir -p test/${names[$i]}
+        for j in {0..299}; do  
+            num=$(printf "%04d" $((no[i] + j)))  # Ensure 4-digit numbering
+            filename="8i_dataset/${names[$i]}/Ply/${names[$i]}_vox10_${num}.ply"
+            output="test/${names[$i]}/tile_${tile_size}_${num}_%04d.ply"
+            ./bin/pcp -i "$filename" -o "$output" -t $tile_size
         done
     done
 done
